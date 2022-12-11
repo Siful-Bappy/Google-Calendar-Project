6bedceb6512c1bce83cba29b0cf58bf81b173297
Siful Bappy <bappy.ac.bd@gmail.com>
2022-12-11 16:12:13 +0900

    send row date to the index complete

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const Day = ({day}) => {
-    console.log(day)
-    return (
-        <div>
-            <h3>{day.format()}</h3>
-        </div>
-    );
+const Day = ({ day, index }) => {
+  console.log(day);
+  return (
+    <div className="border border-gray-200 flex flex-col">
+      <header className="flex flex-col items-center">
+        {
+            index === 0 && <p className="text-sm mt-1">{day.format("ddd")}</p>
+        }
+        
+        <p className="text-sm p-1 my-1 text-center">{day.format("DD")}</p>
+      </header>
+    </div>
+  );
 };
 
 export default Day;
--- src/components/Month.js
+++ src/components/Month.js
@@ -7,9 +7,9 @@ const Month = ({month}) => {
     return (
         <div className='flex-1 grid grid-cols-7 grid-rows-5'>
             {
-                month.map(row => {
+                month.map((row, index) => {
                     return row.map(day => {
-                        return <Day day={day}></Day>
+                        return <Day day={day} index={index}></Day>
                     })
                 })
             }
