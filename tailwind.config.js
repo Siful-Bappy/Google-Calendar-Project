cb2de0e19547a779846750b6e1a50a8a28108925
Siful Bappy <bappy.ac.bd@gmail.com>
2022-12-11 16:24:05 +0900

    complete adding font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans"]
+      },
+      gridTemplateColumns: {
+        "1/5": "1fr 5fr",
+      }
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
