a6f30ec8e390d290a7dfe27b3881e8e892d195de
Siful Bappy <bappy.ac.bd@gmail.com>
2022-12-11 16:39:17 +0900

    display the today with green and round shape style

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,15 +1,19 @@
+import dayjs from "dayjs";
 import React from "react";
 
 const Day = ({ day, index }) => {
-  console.log(day);
+  //   console.log(day);
+  function currentDaycalss() {
+    return day.format("DD-MM-YY") == dayjs().format("DD-MM-YY")
+      ? "bg-blue-600 text-white rounded-full w-7"
+      : "";
+  }
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
-        {
-            index === 0 && <p className="text-sm mt-1">{day.format("ddd")}</p>
-        }
-        
-        <p className="text-sm p-1 my-1 text-center">{day.format("DD")}</p>
+        {index === 0 && <p className="text-sm mt-1">{day.format("ddd")}</p>}
+
+        <p className={`text-sm p-1 my-1 text-center ${currentDaycalss()}`}>{day.format("DD")}</p>
       </header>
     </div>
   );
