e10fde18ed2fda895395f3b8fe403009084f3e0a
Siful Bappy <bappy.ac.bd@gmail.com>
2022-12-11 15:28:52 +0900

    add three more components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import getMonth from "./util";
 // import {getMonth} from "./util";
 import "./App.css";
+import CalendarHeader from "./components/CalendarHeader";
+import Sidebar from "./components/Sidebar";
+import Month from "./components/Month";
+
 
 function App() {
   console.table(getMonth());
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="h-screen flex flex-col">
+      <CalendarHeader></CalendarHeader>
+      <div className="flex flex-1">
+        <Sidebar></Sidebar>
+        <Month></Month>
+      </div>
     </div>
   );
 }
---
+++ src/components/Month.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Month = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Month;
