150886e44bd8e999b2f1427b02a6a560d37c5c6a
Siful Bappy <bappy.ac.bd@gmail.com>
2022-12-17 13:59:18 +0900

    start sidebar task

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -13,12 +13,15 @@ const CalendarHeader = () => {
   const handleNextMonth = () => {
     setMonthIndex(monthIndex + 1)
   }
+  const handleRest = () => {
+    setMonthIndex(dayjs().month())
+  }
 
   return (
     <div className="px-x py-2 flex items-center">
       <img src={logo} alt="Calendar" className="mr-2 w-12 h-12" />
       <h1 className="mr-10 text-xl text-gray-500 font-bold">Calendar</h1>
-      <button className="border rounded py-2 px-4 mr-5">Today</button>
+      <button onClick={handleRest} className="border rounded py-2 px-4 mr-5">Today</button>
       <button onClick={handlePrevMonth}>
         <span className="material-symbols-outlined cursor-pointer text-gray-600 mx-2 ">chevron_left</span>
       </button>
