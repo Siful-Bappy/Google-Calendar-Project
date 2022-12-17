6e05b084d2b4544e98aa26f9595c02fcdd6b9526
Siful Bappy <bappy.ac.bd@gmail.com>
2022-12-17 21:29:34 +0900

    toggle between model

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -3,23 +3,24 @@ import plusImg from "../assets/plus.svg";
 import GlobalContext from "../context/GlobalContext";
 
 const CreateEvent = () => {
-  const { setShowEventModel } = useContext(GlobalContext);
+  const { showEventModel, setShowEventModel } = useContext(GlobalContext);
   const [click, setClick] = useState(false);
+  
   return (
     <div>
       <button
-        onClick={() => setShowEventModel(click)}
+        onClick={() => setShowEventModel(!showEventModel)}
         className="border p-2 rounded-full flex items-center shadow-md hover:shadow-2xl"
       >
         <img src={plusImg} alt="" className="w-7 h-7" />
         <span className="pl-3 pr-7">Create</span>
       </button>
-      <button
+      {/* <button
         onClick={() => setClick(!click)}
         className="rounded border-red-700 p-5 hover:text-red-700"
       >
         {click ? "Remove" : "Click"}
-      </button>
+      </button> */}
     </div>
   );
 };
--- src/components/EventModel.js
+++ src/components/EventModel.js
@@ -1,22 +1,22 @@
-import React, { useContext } from 'react';
-import GlobalContext from '../context/GlobalContext';
+import React, { useContext } from "react";
+import GlobalContext from "../context/GlobalContext";
 
 const EventModel = () => {
-    const {setShowEventModel} = useContext(GlobalContext)
-    return (
-        <div  className='h-screen w-full fixed left-0 top-0 flex justify-center items-center'>
-            <form className='bg-white rounded-lg shadow-2xl w-1/4'>
-                <header className='bg-gray-100 px-4 py-2 flex justify-between item-center'>
-                    <span className='material-icons-outlined text-gray-400'>
-                        drag_handle
-                    </span>
-                    <span className='material-icons-outlined text-gray-400'>
-                        close
-                    </span>
-                </header>
-            </form>
-        </div>
-    );
+  const { setShowEventModel } = useContext(GlobalContext);
+  return (
+    <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
+      <form className="bg-white rounded-lg shadow-2xl w-1/4">
+        <header className="bg-gray-100 px-4 py-2 flex justify-between item-center">
+          <span className="material-icons-outlined text-gray-400">
+            drag_handle
+          </span>
+          <button onClick={() => setShowEventModel(false)}>
+            <span className="material-icons-outlined text-gray-400">close</span>
+          </button>
+        </header>
+      </form>
+    </div>
+  );
 };
 
 export default EventModel;
