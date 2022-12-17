ca3a2fbf57e35d7893bc18ac5122fa958edc6b52
Siful Bappy <bappy.ac.bd@gmail.com>
2022-12-17 17:45:05 +0900

    update the side button also working

--- src/components/SmallCalendar.js
+++ src/components/SmallCalendar.js
@@ -1,26 +1,43 @@
 import dayjs from "dayjs";
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import GlobalContext from "../context/GlobalContext.js";
 import getMonth from "../util.js";
 import "./SmallCalendar.css";
 
 const SmallCalendar = () => {
-  const [currentMonthIndex, setCurrentMonthIndex] = useState(dayjs().month());
-  const [currentMonth, setCurrentMonth] = useState(getMonth());
-  // console.log(currentMonth);
+    const [currentMonthIdx, setCurrentMonthIdx] = useState(
+        dayjs().month()
+      );
+      const [currentMonth, setCurrentMonth] = useState(getMonth());
+      useEffect(() => {
+        setCurrentMonth(getMonth(currentMonthIdx));
+      }, [currentMonthIdx]);
+
+    const {monthIndex} = useContext(GlobalContext);
+    useEffect(() => {
+        setCurrentMonthIdx(monthIndex)
+    }, [monthIndex])
+
+  const handlePrevMonth = () => {
+    setCurrentMonthIdx(currentMonthIdx - 1);
+  }
+  const handleNextMonth = () => {
+    setCurrentMonthIdx(currentMonthIdx + 1);
+  }
   return (
     <div className="mt-9">
       <header className="flex justify-between">
         <p className="text-gray-500 font-bold">
-          {dayjs(new Date(dayjs().year(), currentMonthIndex)).format(
+          {dayjs(new Date(dayjs().year(), currentMonthIdx)).format(
             "MMMM YYYY"
           )}
         </p>
-        <button>
+        <button onClick={handlePrevMonth}>
           <span className="material-symbols-outlined cursor-pointer text-gray-600 ">
             chevron_left
           </span>
         </button>
-        <button>
+        <button onClick={handleNextMonth}>
           <span className="material-symbols-outlined cursor-pointer text-gray-600 ">
             chevron_right
           </span>
